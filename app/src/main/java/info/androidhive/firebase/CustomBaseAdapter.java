00afedbc81412c256f1b074cd5ab2c7121f89f34
CWYBen <u3524056@connect.hku.hk>
2017-03-18 16:52:16 +0800

    Show Gathering

---
+++ app/src/main/java/info/androidhive/firebase/CustomBaseAdapter.java
@@ -0,0 +1,69 @@
+package info.androidhive.firebase;
+
+import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomBaseAdapter extends BaseAdapter {
+    Context context;
+    List<Gathering> rowItems;
+
+    public CustomBaseAdapter(Context context, List<Gathering> items) {
+        this.context = context;
+        this.rowItems = items;
+    }
+
+    /*private view holder class*/
+    private class ViewHolder {
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtDate;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+
+        LayoutInflater mInflater = (LayoutInflater)
+                context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.gathering_list_item, null);
+            holder = new ViewHolder();
+            holder.txtDate = (TextView) convertView.findViewById(R.id.listGatheringDate);
+            holder.txtTitle = (TextView) convertView.findViewById(R.id.listGatheringTitle);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.listGatheringIcon);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Gathering rowItem = (Gathering) getItem(position);
+
+        holder.txtDate.setText(rowItem.getDate());
+        holder.txtTitle.setText(rowItem.getTitle());
+        holder.imageView.setImageResource(R.drawable.logo); //
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return rowItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return rowItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return rowItems.indexOf(getItem(position));
+    }
+}
--- app/src/main/java/info/androidhive/firebase/Tab2.java
+++ app/src/main/java/info/androidhive/firebase/Tab2.java
@@ -8,23 +8,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by shoyu on 16/2/2017.
  */
 
 public class Tab2 extends Fragment{
+
+    private DatabaseReference mGatherings;
+    private List<Gathering> gatheringArrayList = new ArrayList<>();
+    private ListView listView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //Inflate the View first to facilitate findViewById
         View rootView = inflater.inflate(R.layout.tab2, container, false);
 
+        mGatherings = FirebaseDatabase.getInstance().getReference().child("gathering");
+
         FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), AddGatheringActivity.class));
+            }
+        });
+
+        listView = (ListView) rootView.findViewById(R.id.gatheringList);
+        final CustomBaseAdapter adapter = new CustomBaseAdapter(getActivity(), gatheringArrayList);
+        listView.setAdapter(adapter);
+
+        mGatherings.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                gatheringArrayList.clear();
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    String tHolderID, tTitle, tDate, tStartTime, tEndTime, tPlace;
+                    tHolderID = postSnapshot.child("holderID").getValue(String.class);
+                    tTitle = postSnapshot.child("title").getValue(String.class);
+                    tDate = postSnapshot.child("date").getValue(String.class);
+                    tStartTime = postSnapshot.child("startTime").getValue(String.class);
+                    tEndTime = postSnapshot.child("endTime").getValue(String.class);
+                    tPlace = postSnapshot.child("place").getValue(String.class);
+                    gatheringArrayList.add(new Gathering(tHolderID, tTitle, tDate, tStartTime, tEndTime, tPlace));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                throw databaseError.toException();
             }
         });
 
